bc35dc27b15ff2a2acd29656bb1618b3ddae4898
SergeyCherepanov <sergey@cherepanov.org.ua>
2018-03-30 11:59:16 +0400

    update

--- console.js
+++ console.js
@@ -10,16 +10,14 @@ program.version('0.1.0');
 program.command('rewriteuri <uri>')
     .description('my first cli command')
     .action(function(uri) {
-        console.log((function b64Encode(str) {
-            // first we use encodeURIComponent to get percent-encoded UTF-8,
-            // then we convert the percent encodings into raw bytes which
-            // can be fed into btoa.
-            var end = btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
-                function toSolidBytes(match, p1) {
-                    end = String.fromCharCode('0x' + p1);
-                }));
-            return uri + end;
-        })(uri));
+        // first we use encodeURIComponent to get percent-encoded UTF-8,
+        // then we convert the percent encodings into raw bytes which
+        // can be fed into btoa.
+        var end = btoa(encodeURIComponent(uri).replace(/%([0-9A-F]{2})/g,
+            function toSolidBytes(match, p1) {
+                end = String.fromCharCode('0x' + p1);
+            }));
+        console.log(uri + end);
     });
 
 program.parse(process.argv);
